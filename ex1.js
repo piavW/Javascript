c99f4edf264c21f62c54f8e9456a0878f9b32add
piavw <piavonwachenfelt@hotmail.com>
2019-08-19 13:02:59 +0200

    Committing the first exercises for the itp javascript

---
+++ ex1.js
@@ -0,0 +1,21 @@
+//Exercise #1 number 1. 
+numberArray = [1, 2, 3]
+
+//Exercise #1 number 2.
+numberArray[1]
+
+//Exercise #1 number 3. 
+typeof(123/12)
+typeof("Things in quotes?!"
+typeof(undefined)
+
+//Exercise #1 number 4. 
+100 == 50+50
+
+//Exercise #1 number 5. 
+myArray = [“thomas”, “Noel”, “Faraz”, “Pia”, “Julie”]
+emptyArray = []
+for (let i = 0; i <= myArray.length; i++){
+    emptyArray.push(myArray [i]);
+}
+console.log(emptyArray)
---
+++ ex2.js
@@ -0,0 +1,6 @@
+//Exercise #2 number 1. 
+//Write a function that returns your first name. Call it.
+
+
+//Exercise #2 number 2. 
+//Write a new function that takes your name as an input. The function should return your first name, plus your last name, as one string. (Hint: strings can be combined with a +)
